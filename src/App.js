5686a2d42aed707fbfad741d8523720e410ece1a
Ott M. Aaloe <Ott.Aaloe@directo.ee>
2023-11-26 17:34:31 +0200

    ilm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import WeekForecast from './WeekForecast';
+
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState(undefined);
+  const [selectedLocationWeather, setSelectedLocationWeather] = useState(undefined);
+  const [locations, setLocations] = useState([
+    {
+      name: 'Tallinn',
+      latitude: 59.4370,
+      longitude: 24.7536,
+    },
+    {
+      name: 'Bali',
+      latitude: -8.4095,
+      longitude: 115.1889,
+    },
+    {
+      name: 'Rovaniemi',
+      latitude: 66.5039,
+      longitude: 25.7294,
+    },
+
+    
+  ]);
+
+  const selectLocation = (location) => {
+    setSelectedLocation(location);
+    getLocationData(location);
+  }
+
+  const getLocationData = async (location) => {
+    setIsLoading(true);
+    const data = await fetch(`https://api.open-meteo.com/v1/forecast?timezone=auto&latitude=${location.latitude}&longitude=${location.longitude}&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_probability_max,wind_speed_10m_max`);
+    const dataJson = await data.json();
+    setIsLoading(false);
+
+    // tee andmetega midagi...
+    console.log(dataJson);
+    setSelectedLocationWeather(dataJson);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ display: 'flex', flexDirection: 'row', gap: 24, padding: 24 }}>
+        <div style={{ borderRight: '1px solid gray', padding: '0 24px 0 0' }}>
+          <h3>Asukohad</h3>
+          {locations.map((location) => (
+            <div key={location.name}>
+              <a onClick={() => selectLocation(location)}>{location.name}</a>
+            </div>
+          ))}
+        </div>
+        <div style={{ textAlign: 'left' }}>
+          {selectedLocation ? (
+            <>
+              <h3>Ilmateade</h3>
+              <div>
+                <div>{selectedLocation.name}</div>
+                {isLoading ? 'Laen...' : (
+                  <div>
+                    <div>Asukohas {selectedLocation.name} on {selectedLocationWeather.current.temperature_2m}{selectedLocationWeather.current_units.temperature_2m}.</div>
+                    
+                    <WeekForecast dailyData = {selectedLocationWeather.daily} dailyUnits = {selectedLocationWeather.daily_units}  />
+
+                  </div>
+                )}
+
+              </div>
+            </>
+          ) : (
+            <>
+              <h3>Ilmateadet ei ole</h3>
+              <div>Ilmateate vaatamiseks vali linn!</div>
+            </>
+          )}
+        </div>
       </header>
     </div>
   );
---
+++ src/WeekForecast.js
@@ -0,0 +1,43 @@
+import moment from 'moment';
+import 'moment/locale/et';
+import piltSunrise from './pix/sunrise.png';
+import piltSunset from './pix/sunset.png';
+
+const formatDate = (date, fmt) => {
+    moment.locale('et');
+    let ret = moment(date).format(fmt);
+    return (ret);
+}
+
+const WeekForecast = ({ dailyData, dailyUnits }) => (
+    <table cellPadding="10" cellSpacing="0">
+        <thead>
+            <tr>
+                <td>Päev</td>
+                <td><img style={{ width: '50px', padding: '1px' }} src={piltSunrise} /></td>
+                <td><img style={{ width: '50px', padding: '1px' }} src={piltSunset} /></td>
+                <td>Max/Min {dailyUnits.temperature_2m_max}</td>
+                <td>Sademed {dailyUnits.precipitation_probability_max}</td>
+                <td>Tuul m/s</td>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                dailyData.time.map((date, i) => (
+                    
+                    <tr key={i} className={(i % 2) ? 'zebra' : ''}>
+                        {console.log(date)}
+                        <td>{formatDate(date, 'dddd D. MMM')}</td>
+                        <td>{formatDate(dailyData.sunrise[i], 'HH:mm')}</td>
+                        <td>{formatDate(dailyData.sunset[i], 'HH:mm')}</td>
+                        <td align="right">{dailyData.temperature_2m_max[i]} / {dailyData.temperature_2m_min[i]}</td>
+                        <td align="right">{dailyData.precipitation_probability_max[i]}</td>
+                        <td align="right">{parseFloat(dailyData.wind_speed_10m_max[i] * 1000 / 3600, 2).toFixed(1)}</td>
+                    </tr>
+                ))
+            }
+        </tbody>
+    </table>
+)
+
+export default WeekForecast;
